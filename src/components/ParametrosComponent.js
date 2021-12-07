3f14e98ef06e81c55a36f5a3e2d31d6143b66325
eldigardo <heldigard@gmail.com>
2021-12-06 20:15:37 -0500

    add-parametros, add-tipo-cultivo

--- src/components/ParametrosComponent.js
+++ src/components/ParametrosComponent.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import background from './regforms/images/bg-registration-form-2.jpg';
 
 function Parametros() {
-  return <div>Parametros</div>;
+  return (
+    <div class="wrapper" style={{ backgroundImage: `url(${background})` }}>
+      <div class="inner">
+        <form action="">
+          <h3>Formulario Parámetros</h3>
+          <div class="form-group">
+            <div class="form-wrapper">
+              <label for="">Valor Metro Cúbico</label>
+              <input type="text" class="form-control" />
+            </div>
+            <div class="form-wrapper">
+              <label for="">Código Semilla</label>
+              <input type="text" class="form-control" />
+            </div>
+          </div>
+
+          <div class="form-group">
+            <div class="form-wrapper">
+              <label for="">Valor Código Semilla</label>
+              <input type="text" class="form-control" />
+            </div>
+            <div class="form-wrapper">
+              <label for="">Nombre Cultivo</label>
+              <input type="number" class="form-control" />
+            </div>
+          </div>
+
+          <div class="form-wrapper">
+            <label for="">Valor Fertilizantes Kg</label>
+            <input type="number" class="form-control" />
+          </div>
+
+          <button>Guardar Parámetros</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Parametros;
--- src/components/TipoCultivoComponent.js
+++ src/components/TipoCultivoComponent.js
@@ -1,7 +1,47 @@
 import React from 'react';
+import style from './regforms/css/style.css';
+import background from './regforms/images/bg-registration-form-2.jpg';
 
 function TipoCultivo() {
-  return <div>TipoCultivo</div>;
+  return (
+    <div class={style}>
+      <div class="wrapper" style={{ backgroundImage: `url(${background})` }}>
+        <div class="inner">
+          <form action="">
+            <h3>Formulario Tipo Cultivo</h3>
+            <div class="form-group">
+              <div class="form-wrapper">
+                <label for="">Especie Cultivada</label>
+                <input type="text" class="form-control" />
+              </div>
+              <div class="form-wrapper">
+                <label for="">Nombre Cultivo</label>
+                <input type="text" class="form-control" />
+              </div>
+            </div>
+
+            <div class="form-group">
+              <div class="form-wrapper">
+                <label for="">Estación Año</label>
+                <input type="text" class="form-control" />
+              </div>
+              <div class="form-wrapper">
+                <label for="">Código Semilla</label>
+                <input type="number" class="form-control" />
+              </div>
+            </div>
+
+            <div class="form-wrapper">
+              <label for="">Descripción Cultivo</label>
+              <textarea type="text" class="form-control"></textarea>
+            </div>
+
+            <button>Crear Cultivo</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default TipoCultivo;
