fdf3729b0c4f082f75b7815371aa3b4d87c23da1
eldigardo <heldigard@gmail.com>
2021-12-21 10:06:56 -0500

    add-mongoose-package.json-server

--- server/Modelos/index.js
+++ server/Modelos/index.js
@@ -1,12 +1,13 @@
-import mongoose from "mongoose";
-
-import ModelAreas from "./areas_cultivo";
-import ModelTipo from "./tipos_cultivo";
-import ModelProgramacion from "./programacion";
-import ModelUsuarios from "./usuarios";
+import mongoose from 'mongoose';
+import ModelAreas from './areas_cultivo';
+import ModelProgramacion from './programacion';
+import ModelTipo from './tipos_cultivo';
+import ModelUsuarios from './usuarios';
 
 async function main() {
-  await mongoose.connect('mongodb://localhost:27017/test');
+  await mongoose.connect(
+    'mongodb+srv://admin:useradmin@cluster0.he6xv.mongodb.net/argicola?authSource=admin&replicaSet=atlas-ssylfq-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
+  );
 
   return {
     Areas: ModelAreas(mongoose),
